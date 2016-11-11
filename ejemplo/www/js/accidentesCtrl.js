4ddfb7dd08fdc8acd6e45610d0094634aaf86d26
vaskezjohn <vaskezjohn@gmail.com>
2016-11-11 00:54:02 -0300

    agrege unas referencias l index aun no carga en firebase

--- ejemplo/www/js/accidentesCtrl.js
+++ ejemplo/www/js/accidentesCtrl.js
@@ -1,8 +1,26 @@
 angular.module('starter.controllers', ['ionic'])
 
 .controller('AccidentesCtrl', function($scope) {
+  var ref = new Firebase("https://votaciones-f2766.firebaseio.com/");
+  $scope.accidente={};
+  $scope.accidente.nombre;
+  $scope.accidente.direccion;
+  $scope.accidente.tipo;
+  $scope.accidente.telefono;
 
-  
+  $scope.CargarAccidente = function(){
+  	 ref.push({
+      nombre: $scope.nombre,
+      direccion: $scope.direccion,
+      tipo: $scope.tipo,
+      tel: $scope.telefono
+    });
+
+  	$state.go("tab");
+  }
+
+	
+   
 })
 
 
--- ejemplo/www/js/app.js
+++ ejemplo/www/js/app.js
@@ -81,6 +81,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/accidentes/:accidentes-reporte');
+  $urlRouterProvider.otherwise('/tab/accidentes');
 
 });
