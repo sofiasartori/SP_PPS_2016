220ef6a1e0b700e820eb87c6077c1abba0541363
sofiasartori <sofiainessartori@hotmail.com>
2016-11-12 00:31:28 -0300

    muestra animales sueltos en el mapa y llama por telefono
    muestra animales sueltos en el mapa y llama por telefono

--- ejemplo/www/js/animalesCtrl.js
+++ ejemplo/www/js/animalesCtrl.js
@@ -2,17 +2,25 @@ angular.module('starter.controllers', [])
 
 .controller('AnimalesSueltosCtrl', function($scope, $state, $cordovaGeolocation) {
 	var options = {timeout: 10000, enableHighAccuracy: true};
- 
+ 	$scope.setMostrarVaca= function() {
+		document.getElementById("animalSuelto2").style.display = "none";
+		document.getElementById("animalSuelto").style.display = "";
+	}
+	$scope.setMostrarCaballo= function() {
+		document.getElementById("animalSuelto").style.display = "none";
+		document.getElementById("animalSuelto2").style.display = "";
+	}
+
   	$cordovaGeolocation.getCurrentPosition(options).then(function(position){
  
     	var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    	var latLng2 = new google.maps.LatLng(position.coords.latitude+0.001, position.coords.longitude+0.002);
  
     	var mapOptions = {
       		center: latLng,
       		zoom: 15,
       		mapTypeId: google.maps.MapTypeId.ROADMAP
     	};
- 	$scope.mostrarAnimal = false;
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
     google.maps.event.addListenerOnce($scope.map, 'idle', function(){
  
@@ -20,15 +28,30 @@ angular.module('starter.controllers', [])
       	map: $scope.map,
       	animation: google.maps.Animation.DROP,
       	position: latLng
+  		});    
+
+  		var marker2 = new google.maps.Marker({
+      	map: $scope.map,
+      	animation: google.maps.Animation.DROP,
+      	position: latLng2
   		});      
 
   		var infoWindow = new google.maps.InfoWindow({
       		content: "Cuidado!!! Vaca suelta"
   		});
+
+  		var infoWindow2 = new google.maps.InfoWindow({
+      		content: "Cuidado!!! Caballo suelto"
+  		});
  
   		google.maps.event.addListener(marker, 'click', function () {
       		infoWindow.open($scope.map, marker);
-      		$scope.mostrarAnimal=true;
+      		$scope.setMostrarVaca( "");
+  		});
+
+  		google.maps.event.addListener(marker2, 'click', function () {
+      		infoWindow2.open($scope.map, marker2);
+      		$scope.setMostrarCaballo( "");
   		});
  
 	});	
