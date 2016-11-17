aedb1298041ded28db90d1a1adf89199eddfc08b
sofiasartori <sofiainessartori@hotmail.com>
2016-11-17 00:38:07 -0300

    implementado charts
    Fuente: https://jtblin.github.io/angular-chart.js/

--- ejemplo/www/js/animalesCtrl.js
+++ ejemplo/www/js/animalesCtrl.js
@@ -8,6 +8,35 @@ angular.module('starter.controllers', [])
   $scope.animal.longitud;
   $scope.animal.latitud;
 
+  $scope.labels = ["Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre"];
+  $scope.series = ['Caballos', 'Vacas'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+  $scope.options = {
+    scales: {
+      yAxes: [
+        {
+          id: 'y-axis-1',
+          type: 'linear',
+          display: true,
+          position: 'left'
+        },
+        {
+          id: 'y-axis-2',
+          type: 'linear',
+          display: true,
+          position: 'right'
+        }
+      ]
+    }
+  };
+
  	$scope.setMostrarVaca= function() {
 		document.getElementById("animalSuelto2").style.display = "none";
 		document.getElementById("animalSuelto").style.display = "";
