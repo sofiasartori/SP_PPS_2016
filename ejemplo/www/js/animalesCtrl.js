8944a1e8ecedb094ceb87a1e9604c2b7feace3d6
sofiasartori <sofiainessartori@hotmail.com>
2016-11-19 01:59:38 -0300

    vibracion agregada

--- ejemplo/www/js/animalesCtrl.js
+++ ejemplo/www/js/animalesCtrl.js
@@ -1,17 +1,11 @@
 angular.module('starter.controllers', [])
 
-.controller('AnimalesSueltosCtrl', function($scope, $state, $cordovaGeolocation, $ionicPopup, $cordovaVibration, $cordovaNativeAudio){
+.controller('AnimalesSueltosCtrl', function($scope, $state, $cordovaGeolocation, $ionicPopup, $cordovaVibration){
   $scope.$on('cloud:push:notification', function(event, data) {
     var msg = data.message;
     alert(msg.title + ': ' + msg.text);
     $state.go("tab.mapaAnimales");
   });
-  try{
-    $cordovaNativeAudio.preloadSimple('corr', 'sounds/correct.mp3');
-    }
-  catch (e){
-    console.log("no puede reproducir native audio");
-  }
   var options = {timeout: 10000, enableHighAccuracy: true};
   var user = firebase.auth().currentUser;
   var base = new Firebase("https://autopistas-cad17.firebaseio.com/animales");
@@ -20,8 +14,7 @@ angular.module('starter.controllers', [])
   $scope.animal.latitud = '';
 	
   $scope.reportar = function(){
-    //$cordovaNativeAudio.play('corr');
-    //$cordovaVibration.vibrate(100);
+    $cordovaVibration.vibrate(100);
       $cordovaGeolocation.getCurrentPosition(options).then(function(position){
         $scope.animal.longitud=position.coords.latitude; 
         $scope.animal.latitud=position.coords.longitude;
